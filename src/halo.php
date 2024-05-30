09e3f367ed9a570190a6dfb6365b3decf0d4f41b
Muhammad Abel Al-fahrezi <abelalfahrezi11@gmail.com>
2024-05-30 09:38:43 +0700

    commit pertama

--- src/halo.php
+++ src/halo.php
@@ -4,6 +4,6 @@ namespace abel\labhello;
 
 class hello{
     public function tampilHello($nama){
-        return "Hello $nama selamat datang di library marpin";
+        return "Hello $nama selamat datang di library ety";
     }
 }
